5f62bc4da73d6af7bbc69f4a5454c7fa8ce116a3
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-10-16 13:45:18 +0200

    entrar

--- main.js
+++ main.js
@@ -6,4 +6,37 @@ function cambiarImagen() {
         imagen.src = "https://pbs.twimg.com/profile_images/758084549821730820/_HYHtD8F.jpg";
     }
 
-}
+};
+
+
+//1
+
+document.title = 'Ahora trabajando con DOM dinámico';
+
+//2
+
+document.getElementsByTagName('section');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+   
+const sections = document.getElementsByTagName('section');
+[...sections].forEach((section,index) => section.id ='section-' + index);
+console.log(sections);
+
+
+//3
+
+const paragraphs = document.getElementsByTagName('p');
+[...paragraphs].forEach(paragraph => paragraph.style.fontSize = '0.5rem');
+
+//4
+
+const enlaces = document.getElementsByTagName('a');
+[...enlaces].forEach(enlace => enlace.style.textDecoration = 'none');
+
+//5
+
+const boton = document.createElement(input, [checkbox]);
+
+});
